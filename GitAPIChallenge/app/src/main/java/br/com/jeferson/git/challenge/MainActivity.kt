f6402a86fe5138c7a5e828a2262c0dc6b1f3f6cf
Jeferson <joliveira@incastecnologia.com>
2021-03-25 22:10:20 -0300

    Adicionar layout do navigationview. Modificações do gradle

--- GitAPIChallenge/app/src/main/java/br/com/jeferson/git/challenge/MainActivity.kt
+++ GitAPIChallenge/app/src/main/java/br/com/jeferson/git/challenge/MainActivity.kt
@@ -2,17 +2,15 @@ package br.com.jeferson.git.challenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import br.com.jeferson.git.challenge.databinding.MainActivityBinding
 import br.com.jeferson.git.challenge.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.main_activity)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
     }
 }
